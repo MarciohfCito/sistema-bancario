194f9217a582f15dfced51528d830e62f6b0bd7d
Marcio <mchcito@gmail.com>
2026-06-28 22:04:23 -0300

    Criação do repository para CarteiraClientes

---
+++ src/main/java/br/uema/bd/sistema_bancario/repository/CarteirasClientesRepository.java
@@ -0,0 +1,8 @@
+package br.uema.bd.sistema_bancario.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.uema.bd.sistema_bancario.entity.CarteirasClientes;
+
+public interface CarteirasClientesRepository extends JpaRepository<CarteirasClientes, Integer> {
+
+}
