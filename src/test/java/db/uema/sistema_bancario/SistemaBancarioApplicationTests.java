f74406525f1b0e68df492113186a3d5e01dddada
Marcio <mchcito@gmail.com>
2026-06-28 21:10:23 -0300

    first commit

---
+++ src/test/java/db/uema/sistema_bancario/SistemaBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package db.uema.sistema_bancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
